91cc32f002ac438c368b10006ad82e24aa10173c
Prabu Varadharajalu Sivakumar <prabu.sivakumar@icloud.com>
2021-10-14 11:51:00 +0800

    Adding Create and Update APIs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use serde::{Deserialize, Serialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -6,10 +8,8 @@ use solana_program::{
     program_error::ProgramError,
     pubkey::Pubkey,
 };
-use serde::{Deserialize, Serialize};
-use borsh::{BorshDeserialize,BorshSerialize};
 
-#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize,Clone)]
+#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize, Clone)]
 pub struct CustomerData {
     pub instruction: String,
     pub legal_name: String,
@@ -24,10 +24,10 @@ pub struct CustomerData {
     pub swift_code: String,
     pub kyc_status: bool,
     pub addresses: Vec<AddressData>,
-    pub kyc_documents: Vec<KycDocument>
+    pub kyc_documents: Vec<KycDocument>,
 }
 
-#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize,Clone)]
+#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize, Clone)]
 pub struct AddressData {
     pub address_type: String,
     pub address_line1: String,
@@ -35,21 +35,26 @@ pub struct AddressData {
     pub city: String,
     pub state: String,
     pub country: String,
-    pub postal_code: String
+    pub postal_code: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize,Clone)]
+#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize, Clone)]
 pub struct KycDocument {
     pub document_id: String,
     pub document_type: String,
     pub document_name: String,
-    pub document_transaction_id: String
+    pub document_transaction_id: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, BorshDeserialize, BorshSerialize, Clone)]
+pub struct CustomerDataList {
+    pub customers: Vec<CustomerData>,
 }
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    program_id: &Pubkey, 
+    program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
@@ -61,30 +66,62 @@ pub fn process_instruction(
         msg!("Invalid Program Id");
         return Err(ProgramError::IncorrectProgramId);
     }
-    
+
     let data = String::from_utf8(instruction_data.to_vec()).map_err(|_err| {
-                    msg!("Invalid UTF-8, from byte {}");
-                                ProgramError::InvalidInstructionData
+        msg!("Invalid UTF-8, from byte {}");
+        ProgramError::InvalidInstructionData
     })?;
 
     let payload = String::from(data.chars().as_str());
-    msg!("Request Payload is {}",payload);
+    msg!("Request Payload is {}", payload);
     let customer: CustomerData = serde_json::from_str(&payload).unwrap();
-    
+
     match customer.instruction.as_ref() {
-        "POST" => {
-            msg!("POST Operation");  
-             Ok(())
-           },          
-          "GET" => {
+        "CreateCustomer" => {
+            msg!("Creating Customer Data");
+            let mut customers = try_from_slice_unchecked(&account.data.borrow()[..])?;
+            create_customer(customer, &mut customers);
+            msg!("Account Data updated as {:?}", &customers);
+            Ok(BorshSerialize::serialize(
+                &customers,
+                &mut &mut account.data.borrow_mut()[..],
+            )?)
+        }
+        "GetCustomer" => {
             msg!("GET Operation");
-             Ok(())
-           },
-           "PUT" => {
-            msg!("PUT Operation");
-             Ok(())
-           } 
-           _ => Ok(())
-     }
-    
+            Ok(())
+        }
+        "UpdateKycStatus" => {
+            msg!("Updating Customer Data");
+            let mut existing_data = try_from_slice_unchecked(&account.data.borrow()[..])?;
+            update_data(inv_object, &mut existing_data);
+            msg!("Account Data updated as {:?}", &existing_data);
+            Ok(BorshSerialize::serialize(
+                &existing_data,
+                &mut &mut account.data.borrow_mut()[..],
+            )?)
+        }
+        _ => Ok(()),
+    }
+}
+
+pub fn create_customer(customer: CustomerData, customers: &mut CustomerDataList) {
+    msg!("Adding Customer Data {:?}", &customers);
+    customers.data.push(customer);
+}
+
+pub fn update_data(customer: CustomerData, customers: &mut CustomerDataList) {
+    if customer.customer_id == "" || cus.lei == "" {
+        msg!("Matching Customer Data Not Found");
+    } else {
+        let position = existing_data
+            .data
+            .iter()
+            .position(|index| {
+                index.customer_id == customer.customer_id && index.lei == customer.lei
+            })
+            .unwrap();
+        msg!("Matching Customer Data Found in Position {}", position);
+        existing_data.data[position].kyc_status = inv_object.kyc_status;
+    }
 }
